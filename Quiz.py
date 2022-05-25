57ad5e2150e6ddd8a1084b75180921d871977eac
leticia.viana <leticia.viana@hotmail.com>
2022-05-25 20:21:56 -0300

    modificacoes exigidas

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,13 @@
-
-from perguntas import quiz 
 import json
+
+def read_question(dificuldade):
+    with open(f'perguntas-{dificuldade}.txt','r', encoding='utf-8') as f:
+        content = f.readlines()
+        for i in content:
+            split = i.split('|')
+            pergunta = split[0]
+            resposta = split[1]
+            quiz[dificuldade].append({'question': pergunta, 'answer': resposta})
 
 def check_ans(difficult, question, ans, attempts, score):
     if question['answer'].lower() == ans.lower() and difficult == 'facil':
@@ -27,60 +34,79 @@ def intro_message():
     input("\n\n____________Pressione qualquer tecla para começar!____________")
     return True
 
-
-intro = intro_message()
-jogador = str(input("\nDigite o nome do jogador: "))
-difficult= int(input("\n\nEscolha a dificuldade:\n\n 1-FACIL\n\n 2-MÉDIO\n\n 3-DIFICIL\n"))
-if difficult > 3 or difficult == 0:
-    print("Escolha uma dificuldade válida")
-elif difficult == 1:
-    difficult = 'facil'
-elif difficult == 2: 
-    difficult = 'medio'
-elif difficult == 3:
-    difficult = 'dificil'
-
 while True:
-    score = 0
-    for question in quiz[difficult]:
-        attempts = 3
-        while attempts > 0:
-            print(question['question'])
-            answer = input("Digite a alternativa e pressione ENTER (para pular a questão, digite 'skip') : ")
-            if answer == "skip":
-                break
-            check = check_ans(difficult, question, answer, attempts, score)
-            if check:
-                if difficult == 'facil':
-                    score += 1
-                elif difficult == 'medio':
-                    score += 2
-                elif difficult == 'dificil':
-                    score += 3
-                break
-            attempts -= 1
+    quiz = {
+        'facil': [],
+        'normal': [],
+        'dificil': [],
+    }
 
-    break
-print(f"\n\nSua pontuação final é {score}!\n\n Obrigada por jogar!\n\n")
+    read_question('facil')
+    read_question('normal')
+    read_question('dificil')
 
-# [ ] Ler o arquivo players.json
-# [ ] Adicionar o jogar na lista com a pontuação
-# [ ] Escreve o arquivo de novo
+    intro = intro_message()
+    jogador = str(input("\nDigite o nome do jogador: "))
+    difficult= int(input("\n\nEscolha a dificuldade:\n\n 1-FACIL\n\n 2-MÉDIO\n\n 3-DIFICIL\n\n 4-SAIR DO JOGO"))
+    if difficult > 4 or difficult == 0:
+        print("Escolha uma dificuldade válida")
+    elif difficult == 1:
+        difficult = 'facil'
+    elif difficult == 2: 
+        difficult = 'normal'
+    elif difficult == 3:
+        difficult = 'dificil'
+    elif difficult == 4:
+        break
 
-jogadores = None
+    while True:
+        score = 0
+        for question in quiz[difficult]:
+            attempts = 3
+            while attempts > 0:
+                print(question['question'])
+                answer = input("Digite a alternativa e pressione ENTER (para pular a questão, digite 'skip') : ")
+                if answer == "skip":
+                    break
+                check = check_ans(difficult, question, answer, attempts, score)
+                if check:
+                    if difficult == 'facil':
+                        score += 1
+                    elif difficult == 'normal':
+                        score += 2
+                    elif difficult == 'dificil':
+                        score += 3
+                    break
+                attempts -= 1
 
-with open('players.json', 'r') as f:
-    jogadores = json.load(f)
+        break
+    print(f"\n\nSua pontuação final é {score}!\n\n Obrigada por jogar!\n\n")
 
-jogadores.append({
-    'nome': jogador,
-    'pontuacao': score,
-})
+    # [ ] Ler o arquivo players.json
+    # [ ] Adicionar o jogar na lista com a pontuação
+    # [ ] Escreve o arquivo de novo
 
-with open('players.json','w') as f:
-    json.dump(jogadores, f)
+    jogadores = None
 
-print('===RANKING===\n')
+    with open('players.json', 'r') as f:
+        jogadores = json.load(f)
 
-for jogador_dict in jogadores:
-    print(f'{jogador_dict["nome"]} - {jogador_dict["pontuacao"]}')
+    jogadores.append({
+        'nome': jogador,
+        'pontuacao': score,
+    })
+
+    with open('players.json','w') as f:
+        json.dump(jogadores, f)
+
+    print('===RANKING===\n')
+
+    def ranking(jogador): 
+        return jogador['pontuacao']
+
+    jogadores.sort(key=ranking)
+    jogadores = jogadores[::-1]
+
+    for i in range(10):
+        jogador_dict = jogadores[i]
+        print(f'{jogador_dict["nome"]} - {jogador_dict["pontuacao"]}')
